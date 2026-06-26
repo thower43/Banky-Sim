7e0fe3fe63e6f59aaad8250a25f16940eebb51e9
H-Himasha <p.a.h.himasha@gmail.com>
2026-06-26 17:10:06 +0530

    hello

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
+//
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    cout <<"Hello Thamindu";
+    int a = 1;
+    int b = 2;
+    int c = a;
+    a = b;
+    b = c;
+    cout << "a = "<< a << ", " << "b = "<< b;
     return 0;
 }
