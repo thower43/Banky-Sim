f9363c488ca3cbfe731ad0e5c77edcb13cae8180
thower43 <thower46@gmail.com>
2026-06-26 16:51:45 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
+    string wtd = "Nothing";
+    int amount = 0;
+    int Balance = 15000;
     cout << "Welcome To The Banky Bank";
+    cout << "Would You Like To Make A Withdrawl? ";
+    cin >> wtd;
+    if (wtd == "yes") {
+        cout << "How Much Would You Like To Withdrwl? ";
+        cin >> amount;
+        Balance = (Balance - amount);
+        cout << "Successfully Withdrawled"; 
+    } else {
+        cout << "Transaction Failed";
+    }
     return 0;
 }
